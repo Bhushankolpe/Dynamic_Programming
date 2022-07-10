71583650b896000cb883976c436917ef6c91fde7
Bhushankolpe <92618968+Bhushankolpe@users.noreply.github.com>
2022-07-10 13:25:00 +0530

    Add files via upload

---
+++ 17_Maximum Sum of Non-Adjacent Elements_DP.cpp
@@ -0,0 +1,126 @@
+//https://www.codingninjas.com/codestudio/problems/maximum-sum-of-non-adjacent-elements_843261?source=youtube&campaign=striver_dp_videos&utm_source=youtube&utm_medium=affiliate&utm_campaign=striver_dp_videos&leftPanelTab=0
+//You are given an array/list of ‘N’ integers. You are supposed to return the maximum sum of the subsequence with the constraint that no two elements are adjacent in the given array/list.
+#include<iostream>
+#include<vector>
+#include<map>
+#include<unordered_map>
+#include<set>
+#include<stack>
+#include<queue>
+#include<cmath>
+#include<math.h>
+#include<string>
+#include<iterator>
+#include<algorithm>
+#include<ctype.h>
+#define BHUSHAN ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define ll long long
+#define vi vector<ll>
+#define vip vector<pair<ll,ll>>
+#define yes cout<<"YES"<<endl;
+#define no cout<<"NO"<<endl;
+
+using namespace std;
+ll factorial(ll n)
+{return (n == 1 || n == 0) ? 1 : n * factorial(n - 1);}
+ll max(ll a,ll b)
+{
+	if(a>b)
+	return a;
+	return b;
+}
+void inputv(vector<ll>&v,ll n)
+{
+	for(ll i=0;i<n;i++)
+	{
+		ll x;
+		cin>>x;
+		v.push_back(x);
+	}
+	return;
+}
+void coutv(vector<ll>v)
+{
+	for(ll i=0;i<v.size();i++)
+	cout<<v[i]<<" ";
+	cout<<endl;
+	return;
+}
+bool isPowerOfTwo(ll n)
+{if(n==0)
+   return false;
+  return (ceil(log2(n)) == floor(log2(n)));
+}
+bool isPrime(ll n)
+{if (n <= 1)
+    return false;
+    for (int i = 2; i <= sqrt(n); i++)
+        if (n % i == 0)
+            return false;
+    return true;
+}
+int gcd(int a, int b)
+{if (a == 0)return b;return gcd(b % a, a);}
+ll ceil_div(ll a, ll b) {return a % b == 0 ? a / b : a / b + 1;}
+ll mex(vector<ll>v)
+{
+	ll ans=-1;
+	unordered_map<ll,ll>mp;
+	for(ll i=0;i<v.size();i++)
+	mp[v[i]]++;
+	for(ll i=0;i<=v.size();i++)
+	{
+		if(mp[i]==0)
+		return i;
+	}
+
+}
+ bool isSorted(vector<ll>v)
+{    
+      ll n=v.size();
+    vector<ll>s;
+    s=v;
+    sort(s.begin(),s.end());
+    for(ll i=0;i<n;i++)
+    {
+        if(s[i]!=v[i])
+        {
+            return false;
+        }
+    }
+    return true;
+}
+//***************************************START YOUR CODE FROM HERE******************************************//
+// ll arr[1001];
+
+
+int maximumNonAdjacentSum(vector<ll> &nums){
+    // Write your code here.
+    int n=nums.size();
+    vector<int>dp(n+1);
+       int prev2=0;
+       int prev1=nums[0];
+    for(int i=2;i<=n;i++)
+    {
+        int temp=prev1;
+      prev1=max(prev1,prev2+nums[i-1]);
+        prev2=temp;
+    
+    }
+    return prev1;
+}
+int main()
+{
+	BHUSHAN;
+	ll T;
+	cin>>T;
+	while(T--)
+	{
+	   ll n;
+	   cin>>n;
+	   vi v;
+	   inputv(v,n);
+	   cout<<maximumNonAdjacentSum(v)<<endl;
+	}
+	return 0;
+}
