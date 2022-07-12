f3a1b1ec724a3cb38b3cb008e6f4dd459b47749e
Bhushankolpe <92618968+Bhushankolpe@users.noreply.github.com>
2022-07-12 12:55:03 +0530

    Update 8_Count the number of subset with a given difference_DP.cpp

--- 8_Count the number of subset with a given difference_DP.cpp
+++ 8_Count the number of subset with a given difference_DP.cpp
@@ -1,144 +1,52 @@
-#include<iostream>
-#include<vector>
-#include<map>
-#include<unordered_map>
-#include<set>
-#include<stack>
-#include<queue>
-#include<cmath>
-#include<math.h>
-#include<string>
-#include<iterator>
-#include<algorithm>
-#include<ctype.h>
-#define BHUSHAN ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-#define ll long long
-#define vi vector<ll>
-#define vip vector<pair<ll,ll>>
-#define yes cout<<"YES"<<endl;
-#define no cout<<"NO"<<endl;
+#include <bits/stdc++.h>
 
 using namespace std;
-ll factorial(ll n)
-{return (n == 1 || n == 0) ? 1 : n * factorial(n - 1);}
-ll max(ll a,ll b)
-{
-	if(a>b)
-	return a;
-	return b;
-}
-void inputv(vector<ll>&v,ll n)
-{
-	for(ll i=0;i<n;i++)
-	{
-		ll x;
-		cin>>x;
-		v.push_back(x);
-	}
-	return;
-}
-void coutv(vector<ll>v)
-{
-	for(ll i=0;i<v.size();i++)
-	cout<<v[i]<<" ";
-	cout<<endl;
-	return;
-}
-bool isPowerOfTwo(ll n)
-{if(n==0)
-   return false;
-  return (ceil(log2(n)) == floor(log2(n)));
-}
-bool isPrime(ll n)
-{if (n <= 1)
-    return false;
-    for (int i = 2; i <= sqrt(n); i++)
-        if (n % i == 0)
-            return false;
-    return true;
-}
-int gcd(int a, int b)
-{if (a == 0)return b;return gcd(b % a, a);}
-ll ceil_div(ll a, ll b) {return a % b == 0 ? a / b : a / b + 1;}
-ll mex(vector<ll>v)
-{
-	ll ans=-1;
-	unordered_map<ll,ll>mp;
-	for(ll i=0;i<v.size();i++)
-	mp[v[i]]++;
-	for(ll i=0;i<=v.size();i++)
-	{
-		if(mp[i]==0)
-		return i;
-	}
 
-}
- bool isSorted(vector<ll>v)
-{    
-      ll n=v.size();
-    vector<ll>s;
-    s=v;
-    sort(s.begin(),s.end());
-    for(ll i=0;i<n;i++)
-    {
-        if(s[i]!=v[i])
-        {
-            return false;
+int mod =(int)1e9+7;
+
+int findWays(vector<int> &num, int tar){
+     int n = num.size();
+
+    vector<vector<int>> dp(n,vector<int>(tar+1,0));
+    
+    if(num[0] == 0) dp[0][0] =2;  // 2 cases -pick and not pick
+    else dp[0][0] = 1;  // 1 case - not pick
+    
+    if(num[0]!=0 && num[0]<=tar) dp[0][num[0]] = 1;  // 1 case -pick
+    
+    for(int ind = 1; ind<n; ind++){
+        for(int target= 0; target<=tar; target++){
+            
+            int notTaken = dp[ind-1][target];
+    
+            int taken = 0;
+                if(num[ind]<=target)
+                    taken = dp[ind-1][target-num[ind]];
+        
+            dp[ind][target]= (notTaken + taken)%mod;
         }
     }
-    return true;
+    return dp[n-1][tar];
 }
-//***************************************START YOUR CODE FROM HERE******************************************//
-// ll arr[1001];
 
-
-ll ok(vi v,ll n,ll t)
-{
-	ll dp[n+1][t+1];
-	for(ll j=0;j<=t;j++)
-	dp[0][j]=0;
-	for(ll i=0;i<n+1;i++)
-	dp[i][0]=1;
-	for(ll i=1;i<n+1;i++)
-	{
-		for(ll j=1;j<t+1;j++)
-		{
-			if(v[i-1]<=j)
-			{
-				dp[i][j]=dp[i-1][j]+dp[i-1][j-v[i-1]];
-			}
-			else
-			dp[i][j]=dp[i-1][j];
-		}
-	}
-	return dp[n][t];
+int countPartitions(int n, int d, vector<int>& arr){
+    int totSum = 0;
+    for(int i=0; i<n;i++){
+        totSum += arr[i];
+    }
+    
+    //Checking for edge cases
+    if(totSum-d <0 || (totSum-d)%2 ) return 0;
+    
+    return findWays(arr,(totSum-d)/2);
 }
-int main()
-{
-	BHUSHAN;
-	ll T;
+  
 
-	T=1;
-	while(T--)
-	{
+int main() {
 
-	   ll n;
-	   cin>>n;
-
-	    vi v;
-	    inputv(v,n);
-
-	    ll diff;
-	    cin>>diff;
-	    ll sum=0;
-	    for(ll i=0;i<n;i++)
-	    {
-	    	sum=sum+v[i];
-		}
-		ll t=(sum+diff)/2;
-	    
-	    cout<<"The number of subset of given difference is: "<<ok(v,n,t)<<endl;
-	    
-	}
-	return 0;
+  vector<int> arr = {5,2,6,4};
+  int n = arr.size();
+  int d=3;
+                                 
+  cout<<"The number of subsets found are " <<countPartitions(n,d,arr);
 }
