b3df25d5939471f8d90d32625ab1526ca5cb007f
Bhushankolpe <92618968+Bhushankolpe@users.noreply.github.com>
2022-07-15 09:40:30 +0530

    Add files via upload

---
+++ 30_Longest_Palindromic_Subsequence_DP.cpp
@@ -0,0 +1,130 @@
+#include<iostream>
+#include<vector>
+#include<map>
+#include<unordered_map>
+#include<set>
+#include<stack>
+#include<queue>
+#include<cmath>
+#include<math.h>
+#include<string>
+#include<iterator>
+#include<algorithm>
+#include<ctype.h>
+#define BHUSHAN ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define ll long long
+#define vi vector<ll>
+#define vip vector<pair<ll,ll>>
+#define yes cout<<"YES"<<endl;
+#define no cout<<"NO"<<endl;
+
+using namespace std;
+ll factorial(ll n)
+{return (n == 1 || n == 0) ? 1 : n * factorial(n - 1);}
+ll max(ll a,ll b)
+{
+	if(a>b)
+	return a;
+	return b;
+}
+void inputv(vector<ll>&v,ll n)
+{
+	for(ll i=0;i<n;i++)
+	{
+		ll x;
+		cin>>x;
+		v.push_back(x);
+	}
+	return;
+}
+void coutv(vector<ll>v)
+{
+	for(ll i=0;i<v.size();i++)
+	cout<<v[i]<<" ";
+	cout<<endl;
+	return;
+}
+bool isPowerOfTwo(ll n)
+{if(n==0)
+   return false;
+  return (ceil(log2(n)) == floor(log2(n)));
+}
+bool isPrime(ll n)
+{if (n <= 1)
+    return false;
+    for (int i = 2; i <= sqrt(n); i++)
+        if (n % i == 0)
+            return false;
+    return true;
+}
+int gcd(int a, int b)
+{if (a == 0)return b;return gcd(b % a, a);}
+ll ceil_div(ll a, ll b) {return a % b == 0 ? a / b : a / b + 1;}
+ll mex(vector<ll>v)
+{
+	ll ans=-1;
+	unordered_map<ll,ll>mp;
+	for(ll i=0;i<v.size();i++)
+	mp[v[i]]++;
+	for(ll i=0;i<=v.size();i++)
+	{
+		if(mp[i]==0)
+		return i;
+	}
+
+}
+ bool isSorted(vector<ll>v)
+{    
+      ll n=v.size();
+    vector<ll>s;
+    s=v;
+    sort(s.begin(),s.end());
+    for(ll i=0;i<n;i++)
+    {
+        if(s[i]!=v[i])
+        {
+            return false;
+        }
+    }
+    return true;
+}
+//***************************************START YOUR CODE FROM HERE******************************************//
+// ll arr[1001];
+
+
+int longestPalindromeSubseq(string s) {
+        int n=s.size();
+        string s1=s;
+        reverse(s1.begin(),s1.end());
+        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+        for(int i=1;i<=n;i++)
+        {
+
+        for(int j=1;j<=n;j++)
+        {
+
+         if(s[i-1]==s1[j-1])
+         {
+           dp[i][j]=1+dp[i-1][j-1];
+         
+         }
+            else
+                dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+        }
+        }
+        return dp[n][n];
+    }
+int main()
+{
+	BHUSHAN;
+	ll T;
+	cin>>T;
+	while(T--)
+	{
+		string s;
+		cin>>s;
+	   
+	   cout<<longestPalindromeSubseq(s)<<endl;
+	}
+	return 0;
+}
